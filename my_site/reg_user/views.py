553f071d5095481ad5d118986063499ee5721653
ilya.devel <ilya.devel@yandex.ru>
2023-12-15 09:32:23 +0000

    complete reg service

--- my_site/reg_user/views.py
+++ my_site/reg_user/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from . import forms
+from django.contrib.auth.models import User
+from django.contrib import auth
 import logging
 
 
@@ -10,7 +13,16 @@ def reg(request):
     if request.method == 'POST':
         form = forms.RegUserForm(request.POST)
         if form.is_valid():
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
             logger.info('User created')
+            new_user = auth.authenticate(username=username, password=password)
+            if new_user is not None:
+                auth.login(request, new_user)
+                return HttpResponseRedirect("/")           
     context = {
         'form': form,
     }
