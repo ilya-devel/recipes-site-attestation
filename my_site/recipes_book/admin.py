7d5cc72b057dff8a54ae8fb28c56d5b2b8fa55d0
ilya.devel <ilya.devel@yandex.ru>
2023-12-16 09:01:08 +0000

    first versions models Category and Recipe

--- my_site/recipes_book/admin.py
+++ my_site/recipes_book/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['title', 'make_time', 'author', 'create_date',
+                'update_date', 'is_public', 'is_active']
+    list_filter = ['create_date', 'update_date']
+    ordering = ['-update_date']
+    readonly_fields = ['create_date', 'update_date']
+
+    fieldsets = [
+        (
+            None,
+            {
+                'classes': ['wide'],
+                'fields': ['title', 'description', 'category']
+            }
+        ),
+        (
+            'О рецепте',
+            {
+                'classes': ['collapse', 'wide'],
+                'fields': ['steps', 'make_time', 'image', 'author']
+            }
+        ),
+        (
+            'Статус',
+            {
+                'classes': ['wide'],
+                'fields': ['create_date', 'update_date', 'is_public', 'is_active']
+            }
+        )
+    ]
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    ordering = ['name']
--- my_site/recipes_book/models.py
+++ my_site/recipes_book/models.py
@@ -1,3 +1,26 @@
+from email.policy import default
+from unicodedata import category
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=None)
+    description = models.TextField()
+    steps = models.TextField()
+    make_time = models.DurationField(default=None)
+    image = models.ImageField(upload_to='photo/')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    is_public = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
