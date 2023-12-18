3b75bbc88b84ace781b7175b23aa6ee21fc9e401
ilya.devel <ilya.devel@yandex.ru>
2023-12-18 17:29:55 +0000

    complete index page

--- my_site/recipes_book/views.py
+++ my_site/recipes_book/views.py
@@ -2,12 +2,23 @@ import logging
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.contrib.auth.decorators import login_required
 from . import models
+from random import choice
 
 logger = logging.getLogger(__name__)
 
 
 def index(request):
+    recipes = models.Recipe.objects.all()
+    result = []
+    if len(recipes) > 5:
+        while len(result) < 5:
+            tmp = choice(recipes)
+            if tmp not in result:
+                result.append(tmp)
+    else:
+        result = recipes
     context = {
+        'recipes': result,
     }
     return render(request, 'recipes_book/index.html', context=context)
 
